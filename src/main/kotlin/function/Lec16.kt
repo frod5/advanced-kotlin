686e9eaf40ef08e63fd53e35cbdef030b0e006db
김선우 <frod5@kakao.com>
2025-03-26 17:30:40 +0900

    kotlin advanced - sam, reference

--- src/main/kotlin/function/Lec16.kt
+++ src/main/kotlin/function/Lec16.kt
@@ -1,5 +1,12 @@
 package function
 
+/**
+ * SAM
+ * single abstract method
+ * 1개의 추상메소드만 가진 인터페이스 -> SAM 인터페이스
+ * java에서는 sam 인터페이스를 람다(자바의 람다)로 인스턴스화 할 수 있다.
+ */
+
 fun add(a: Int, b: Int) = a + b
 
 fun main() {
@@ -8,7 +15,7 @@ fun main() {
 
   val add2 = fun (a: Int, b: Int) = a + b
 
-  ::add
+  ::add // 호출가능참조
 
   //KStringFilter { it.startsWith("A") }
 }
@@ -17,6 +24,7 @@ fun consumeFilter(filter: StringFilter) { }
 
 fun <T> consumeFilter(filter: Filter<T>) {}
 
+// kotlin의 sam 인터페이스
 fun interface KStringFilter {
   fun predicate(str: String): Boolean
 }
