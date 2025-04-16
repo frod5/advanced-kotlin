94c5aa2bf8bfe65cc94aaa55fcc03027862372bc
김선우 <frod5@kakao.com>
2025-04-16 17:12:59 +0900

    kotlin advanced - kotlin annotation

--- src/main/kotlin/reflection/Annotation.kt
+++ src/main/kotlin/reflection/Annotation.kt
@@ -4,10 +4,13 @@ import kotlin.reflect.KClass
 
 @Repeatable
 @Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class Shape(
   val texts: Array<String>
 )
 
+
+//@Repeatable 선언 시 여러 번 사용 가능
 @Shape(["C"])
 @Shape(["A", "B"])
 class Annotation {
